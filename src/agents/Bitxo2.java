8a834c06ed286c2a9541b032c11f42c5ec2a0a59
Joan López Ferrer <trombonet12@gmail.com>
2021-10-28 18:22:57 +0200

    Sistemes de Combat Acabats y millores generals

--- src/agents/Bitxo2.java
+++ src/agents/Bitxo2.java
@@ -10,161 +10,221 @@ public class Bitxo2 extends Agent {
     static final int ESQUERRA = 0;
     static final int CENTRAL = 1;
     static final int DRETA = 2;
-    final double dist = 90; //camina v1 a 80
-    final double distL = 30;
-    int repetir;
-    int repetirC;
-    int gana; //temps sense menjar
 
+    //Torna el valor de totes les variables d'entorn.
     Estat estat;
+    int minim, minim2;
+    int repetirMirar, repetirMirar2;
+    boolean seguimColisio;
+    int posicioArray;
+    int posicioArray2;
+    int tencGana;
+    int noVeigRes;
+    boolean faigVolta;
 
     public Bitxo2(Agents pare) {
-        super(pare, "Sombra", "imatges/robotank1.gif");
+        super(pare, "Perry", "imatges/robotank3.gif");
     }
 
     @Override
     public void inicia() {
         // atributsAgents(v,w,dv,av,ll,es,hy)
-        int cost = atributsAgent(6, 5, 600, 30, 23, 5, 5);
+        int cost = atributsAgent(5, 4, 650, 40, 26, 5, 4);
         System.out.println("Cost total:" + cost);
 
         // Inicialització de variables que utilitzaré al meu comportament
-        repetir = 0;
-        repetirC = 0;
-        gana = 0;
-    }
-
-    private void camina() {
-        if ((estat.distanciaVisors[CENTRAL] < dist) && (estat.objecteVisor[CENTRAL] == PARET)) {
-            if (estat.distanciaVisors[DRETA] < estat.distanciaVisors[ESQUERRA]) {
-                gira(10);
-            } else if (estat.distanciaVisors[DRETA] > estat.distanciaVisors[ESQUERRA]) {
-                gira(350);
-            }
-        } else if ((estat.distanciaVisors[DRETA] < distL) && (estat.objecteVisor[DRETA] == PARET)) {
-            gira(5);
-        } else if ((estat.distanciaVisors[ESQUERRA] < distL) && (estat.objecteVisor[ESQUERRA] == PARET)) {
-            gira(355);
-        }
-
-        if (estat.enCollisio == true) {
-            if (repetir > 40) {
-                hyperespai();
-                repetir = 0;
-            } else if (repetir > 20) {
-                endavant();
-                repetir = 0; //PROVISIONAL  
-            } else {
-                enrere();
-                repetirC = 10;
-                repetir += 5;
-            }
-        }
-
-        System.out.println(repetir);
-        if (repetir == 0) {
-            atura();
-            endavant();
-            System.out.println("endevant");
-        } else {
-            repetir--;
-        }
-    }
-
-    private boolean cerca() {
-        int min = Integer.MAX_VALUE;
-        int proper = -1;
-        for (int i = 0; i < estat.numObjectes; i++) {
-            //if ((estat.objectes[i].agafaDistancia() < min)) {
-            //if ((estat.objectes[i].agafaTipus() != -1) && (estat.objectes[i].agafaDistancia() < min)) {
-            //if ((estat.objectes[i].agafaTipus() == (estat.id + 100)) && (estat.objectes[i].agafaDistancia() < min) && ((estat.objectes[i].agafaSector() == 2) || (estat.objectes[i].agafaSector() == 3))) {
-            if (((estat.objectes[i].agafaSector() == 2) || (estat.objectes[i].agafaSector() == 3))) {
-                if (recursInteresant(estat.objectes[i])) {
-                    if (estat.objectes[i].agafaDistancia() < min) {
-                        min = estat.objectes[i].agafaDistancia();
-                        proper = i;
-                    }
-                }
-            }
-        }
-        System.out.println(repetirC);
-        if (proper != -1) {
-            if (repetirC == 0) {
-                mira(estat.objectes[proper]);
-                System.out.println("Found it");
-            } else {
-                repetirC--;
-            }
-            gana = 0;
-            return true;
-        }
-        return false;
-    }
-
-    private boolean recoleccio() {
-        if (!cerca()) {
-            gana++;
-            System.out.println("Gana: " + gana);
-            if (gana > 30) {
-                if (gana > 39) {
-                    gana = 0;
-                }
-                atura();
-                gira(36);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean recursInteresant(Objecte objecte) {
-        if (objecte.agafaTipus() >= 100) {
-            if (objecte.agafaTipus() == (estat.id + 100)) {
-                return true;
-            } else {
-                if (objecte.agafaDistancia() <= 400) {
-                    mira(objecte);
-                    llança();
-                }
-                return false;
-            }
-        } else if (objecte.agafaTipus() == Estat.ESCUT) {
-            return (estat.escuts < 3);
-            //PROVISIONAL    
-        } else if (objecte.agafaTipus() == Estat.AGENT) {
-            if (objecte.agafaDistancia() <= 100) {
-                    mira(objecte);
-                    llança();
-                }
-            return false;
-        }
-        return false;
-    }
-
-    private void defensa() {
-        if (estat.llançamentEnemicDetectat && (estat.distanciaLlançamentEnemic < 50)) {
-            activaEscut();
-        }
+        minim = Integer.MAX_VALUE;
+        minim2 = Integer.MAX_VALUE;
+        repetirMirar = 2;
+        repetirMirar2 = 2;
+        tencGana = 45;
+        seguimColisio = false;
+        noVeigRes = 0;
+        faigVolta = false;
     }
 
     @Override
     public void avaluaComportament() {
         estat = estatCombat();
-        defensa();
-        if (!recoleccio()) {
-            camina();
+        disparEnemicDetectat();
+        recolectar();
+        if(!faigVolta){
+            System.out.println("CAMINAM");
+            faigVolta = false;
+            camina(50);
+        }
+        
+
+    }
+
+    //MÈTODES RELACIONATS AMB EL MOVIMENT.
+    private void camina(int distancia) {
+        if (estat.enCollisio == true) {
+            if (!seguimColisio) {
+                System.out.println("COLISIO");
+                enrere();
+                seguimColisio = true;
+            } else {
+                System.out.println("doble colisio");
+                endavant();
+                seguimColisio = false;
+            }
+        } else {
+            seguimColisio = false;
+        }
+        if (hiHaParet(distancia)) {
+            System.out.println("hihaParet, no sabem on");
+            sortida();
+        } else {
+            System.out.println("No hi ha paret, endavant");
+            endavant();
+        }
+    }
+
+    private boolean hiHaParet(int distancia) {
+
+        return hiHaParentPerVisor(CENTRAL, distancia) || hiHaParentPerVisor(DRETA, distancia) || hiHaParentPerVisor(ESQUERRA, distancia);
+    }
+
+    private boolean hiHaParentPerVisor(int visor, int distancia) {
+        return estat.distanciaVisors[visor] <= distancia && estat.objecteVisor[visor] == PARET;
+    }
+
+    private void sortida() {
+
+        if (estat.distanciaVisors[DRETA] < estat.distanciaVisors[ESQUERRA]) {
+            System.out.println("Cap a l'esquerra");
+            gira(10);
+            endavant();
+        } else if (estat.distanciaVisors[ESQUERRA] < estat.distanciaVisors[DRETA]) {
+            System.out.println("Cap a la dreta");
+            gira(-10);
+            endavant();
+        }
+    }
+
+    //MÈTODES RELACIONATS AMB LA RECOLECCIÓ D'ALIMENTS I ESCUTS.
+    private void recolectar() {
+
+        // System.out.println("dins recolectar ");
+        if (!observar() && estat.recursosAgafats <= 5) {
+            tencGana--;
+            faigVolta = false;
+            if (tencGana <= 9) {
+                System.out.println("sonar:" + tencGana);
+                if (tencGana == 0) {
+                    tencGana = 45;
+                }
+                atura();
+                gira(36);
+                faigVolta = true;
+            }
+            //Hyper pq no troba res nomes un pic
+            if (noVeigRes == 300 && estat.recursosAgafats >= 4) {
+                noVeigRes = 0;
+                if (estat.hiperespais > 0 && (estat.forces >= 11000 || estat.recursosAgafats < 4)) {
+                    if (estat.forces > 6000 && !estat.hiperEspaiActiu) {
+                        hyperespai();
+                    }
+                }
+            }
+        } else {
+            tencGana = 45;
+            noVeigRes = 0;
+            faigVolta = false;
+        }
+    }
+
+    private boolean observar() {
+        //  System.out.println("Dins observar");
+        posicioArray = -1;
+        for (int i = 0; i < estat.numObjectes; i++) {
+            if (esRecursPersonal(i)) {
+                //  System.out.println("El recurs es positiu");
+                if (menorDistancia(i, minim)) {
+                    minim = estat.objectes[i].agafaDistancia();
+                    posicioArray = i;
+                }
+            } else if (noEsRecursPersonal(i)) {
+                //  System.out.println("El recurs es negatiu");
+                if (repetirMirar2 == 0) {
+                    repetirMirar2 = 2;
+                    mira(estat.objectes[i]);
+                    if (!estat.llançant) {
+                        llança();
+                    }
+                    noVeigRes = 0;
+                } else {
+                    repetirMirar2--;
+                }
+            }
+        }
+        minim = Integer.MAX_VALUE;
+        if (posicioArray != -1) {
+            if (repetirMirar == 0) {
+                repetirMirar = 2;
+                //  System.out.println("MIRAM");
+                mira(estat.objectes[posicioArray]);
+                noVeigRes = 0;
+            } else {
+                //System.out.println("decrementam mirar");
+                repetirMirar--;
+            }
+            return true;
+        }
+        // System.out.println("NO VEIG RES VAL: " + noVeigRes);
+        noVeigRes++;
+        return false;
+
+    }
+
+    private boolean esRecursPersonal(int i) {
+
+        if ((estat.objectes[i].agafaSector() == 2
+                || estat.objectes[i].agafaSector() == 3)) {
+            if (estat.objectes[i].agafaTipus() == 100 + estat.id) {
+                return true;
+            } else if (estat.objectes[i].agafaTipus() == Estat.ESCUT) {
+                return (estat.recursosAgafats >= 5);
+            }
+        }
+        return false;
+    }
+
+    private boolean noEsRecursPersonal(int i) {
+        return ((estat.objectes[i].agafaSector() == 2 || estat.objectes[i].agafaSector() == 3) && (estat.objectes[i].agafaTipus() >= 100)
+                && (estat.objectes[i].agafaTipus() != (100 + estat.id)) && (estat.objectes[i].agafaDistancia() <= 350));
+    }
+
+    private boolean menorDistancia(int i, int minim) {
+        return minim > estat.objectes[i].agafaDistancia();
+    }
+
+    private void disparEnemicDetectat() {
+
+        if (estat.llançamentEnemicDetectat && estat.distanciaLlançamentEnemic <= 100) {
+            if (estat.escuts > 0 && !estat.escutActivat) {
+                activaEscut();
+            } else if (estat.hiperespais > 0 && !estat.hiperEspaiActiu) {
+                hyperespai();
+            }
         }
 
     }
 
+    /*
+            if (estat.llançamentEnemicDetectat && estat.distanciaLlançamentEnemic < 100) {
+            if (estat.hiperespais > 0 && estat.hiperespais >= estat.escuts && estat.forces >= 10000) {
+                hyperespai();
+                System.out.println("hyper perque condicons favorbales");
+            } else if (estat.escuts > 0) {
+                activaEscut();
+                System.out.println("Escut perque hyper no conve");
+            } else if (estat.hiperespais > 0) {
+                //Cas hyper > estats i forces < 4500
+                System.out.println("Hyper pq no donam per mes");
+                hyperespai();
+            }
+        }
+     */
 }
-
-/*
-COSES A FER
-    -Si se bloqueja envant y enrere tirar hiperespai. 
-    -Sistema de recolecció d'escuts en base als que tens // DONE
-    -Sistema de destruccio d'elements enemics
-    -Sistema de combat
-        -Sistema de defensa
-        -Sistema d'atac.
- */
